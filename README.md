e37a155dc5e18f018017589da1bfdb240aaee65f
LCL121 <1391436522@qq.com>
2020-10-08 13:31:55 +0800

    Front end initialization completed

---
+++ README.md
@@ -0,0 +1,9 @@
+# 前端使用
+
+前提是安装了node
+
+1. cd ./restaurant-management-system-front-end
+2. npm install 安装依赖
+3. npm run serve 开发
+4. npm run build 打包
+
---
+++ restaurant-management-system-front-end/vue.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  publicPath: process.env.VUE_APP_BASE_API,
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = '餐厅系统'
+        return args
+      })
+
+    if (process.env.NODE_ENV === 'development') {
+      config.module
+        .rule('eslint')
+        .use('eslint-loader')
+        .loader('eslint-loader')
+        .tap(options => {
+          options.fix = true
+          return options
+        })
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: path.join(__dirname, 'src/assets'),
+        utils: path.join(__dirname, 'src/utils')
+      }
+    }
+  }
+}
